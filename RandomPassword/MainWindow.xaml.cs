6e2ebebb49a90e3fa18693f7b80a9490834a9063
666obituary666 <smolin_zhenia@mail.ru>
2021-12-13 22:46:20 +0900

    Добавьте файлы проекта.

---
+++ RandomPassword/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomPassword
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnPassword_Click(object sender, RoutedEventArgs e)
+        {
+            string ipassword = "";
+            string[] array = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "A", "E", "U", "Y", "a", "e", "i", "o", "u", "y" };
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i = i + 1)
+            {
+                ipassword = ipassword + array[rnd.Next(0, 57)];
+            }
+            TextBox.Text = ipassword;
+        }
+    }
+}
